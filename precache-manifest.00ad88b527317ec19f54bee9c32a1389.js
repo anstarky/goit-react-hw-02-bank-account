94031982b5b1bcc3b33e66e266279712aa798c74
“anstarky” <anstarky@gmail.com>
2019-11-16 13:49:10 +0200

    Updates

---
+++ precache-manifest.00ad88b527317ec19f54bee9c32a1389.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "303e018ab4eeec4ecbbcacf5b91c82c8",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "ff66a7048ac66cad25ee",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.da529cc7.chunk.css"
+  },
+  {
+    "revision": "ac5ffb6805193b90dbac",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.da1ffc57.chunk.css"
+  },
+  {
+    "revision": "ff66a7048ac66cad25ee",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.8bb4334a.chunk.js"
+  },
+  {
+    "revision": "ac5ffb6805193b90dbac",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.fe66d5fa.chunk.js"
+  },
+  {
+    "revision": "f62f12dd8357cc87dfa5",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.202bf4ec.js"
+  }
+]);
