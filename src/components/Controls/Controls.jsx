f9067507954379798b4211a94b3270d80020ea7d
“anstarky” <anstarky@gmail.com>
2019-11-16 13:47:33 +0200

    add homework 02-2

---
+++ src/components/Controls/Controls.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+
+import styles from './Controls.module.css';
+
+class Controls extends Component {
+    state = {
+        amount: '',
+    };
+
+    handleInputChange = e => {
+        this.setState({
+            amount: Number(e.currentTarget.value),
+        });
+    };
+
+    handleClickDeposit = e => {
+        e.preventDefault();
+        this.props.onDeposit(this.state.amount);
+        this.setState({ amount: '' });
+    };
+
+    handleClickWithdraw = e => {
+        e.preventDefault();
+
+        this.props.onWithdraw(this.state.amount);
+        this.setState({ amount: '' });
+    };
+
+    render() {
+        const { amount } = this.state;
+        return (
+            <section className={styles.controls}>
+                <input
+                    type="number"
+                    name="amount"
+                    className={styles.controls__input}
+                    value={amount}
+                    onChange={this.handleInputChange}
+                />
+                <button
+                    type="button"
+                    className={styles.controls__button}
+                    onClick={this.handleClickDeposit}
+                >
+                    Deposit
+                </button>
+                <button
+                    type="button"
+                    className={styles.controls__button}
+                    onClick={this.handleClickWithdraw}
+                >
+                    Withdraw
+                </button>
+            </section>
+        )
+    }
+};
+
+export default Controls;
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import T from 'prop-types';
+
+import styles from './TransactionHistory.module.css';
+
+const TransactionHistory = ({ items }) => (
+    <table className={styles.history}>
+        <thead className={styles.history__head}>
+            <tr>
+                <th>Transaction</th>
+                <th>Amount</th>
+                <th>Date</th>
+            </tr>
+        </thead>
+        <tbody>
+            {items.map(item => (
+                <tr key={item.id}>
+                    <td>{item.type}</td>
+                    <td>{item.amount.toFixed(2)}$</td>
+                    <td>{item.date}</td>
+                </tr>
+            ))}
+        </tbody>
+    </table>
+);
+
+TransactionHistory.propTypes = {
+    items: T.arrayOf(
+        T.shape({
+            id: T.string.isRequired,
+            type: T.string.isRequired,
+            amount: T.number.isRequired,
+            date: T.string.isRequired,
+        }).isRequired,
+    ).isRequired,
+};
+
+export default TransactionHistory;
